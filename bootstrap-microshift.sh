f343c55f232f8aefc52c393aa573370e02188145
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-10 20:34:06 +0200

    Hook up microshift to gitops repo

---
+++ bootstrap-microshift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+OPERATOR_SDK_VERSION="v1.20.0"
+
+curl -L -o /usr/local/bin/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk_linux_amd64
+chmod a+x /usr/local/bin/operator-sdk
+
+cp -rvf /var/lib/microshift/resources/kubeadmin/kubeconfig /tmp/kubeconfig
+export KUBECONFIG=/tmp/kubeconfig
+operator-sdk olm install
+
+oc create -f https://operatorhub.io/install/argocd-operator.yaml
+
+oc apply -f /etc/microshift/manifests/argocg-settings.yaml
+oc apply -f /etc/microshift/manifests/cluster-app.yaml
--- install-microshift.sh
+++ install-microshift.sh
@@ -6,6 +6,7 @@ curl -L -o /etc/yum.repos.d/fedora-updates-modular.repo https://src.fedoraprojec
 curl -L -o /etc/yum.repos.d/group_redhat-et-microshift-fedora-35.repo https://copr.fedorainfracloud.org/coprs/g/redhat-et/microshift/repo/fedora-35/group_redhat-et-microshift-fedora-35.repo
 rpm-ostree ex module enable cri-o:1.21
 rpm-ostree install cri-o cri-tools microshift
+systemctl enable microshift
 
 curl -L https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.10/openshift-client-linux-4.10.12.tar.gz -o /tmp/oc.tar.gz
 tar xzf /tmp/oc.tar.gz -C /usr/local/bin/
