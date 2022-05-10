37fe893367973bb1cf00e7effc3894ca775a67bd
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-10 19:01:27 +0200

    run-coreos-installer.sh: install on nvme disk

--- run-coreos-installer.sh
+++ run-coreos-installer.sh
@@ -3,15 +3,7 @@ set -x
 
 ignition_file='/var/lib/microshift.ign'
 firstboot_args='console=tty0'
-if [ -b /dev/sda ]; then
-    install_device='/dev/sda'
-elif [ -b /dev/nvme0 ]; then
-    install_device='/dev/nvme0'
-else
-    echo "Can't find appropriate device to install to" 1>&2
-    poststatus 'failure'
-    return 1
-fi
+install_device='/dev/nvme0n1'
 cmd="coreos-installer install --firstboot-args=${firstboot_args}"
 cmd+=" --ignition=${ignition_file}"
 cmd+=" ${install_device}"
