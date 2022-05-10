be8e3fb3384bcbde68afe116fb7344276a298e86
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-10 19:17:33 +0200

    install-microshift: remove extra param from rpm-ostree install

--- install-microshift.sh
+++ install-microshift.sh
@@ -5,5 +5,5 @@ curl -L -o /etc/yum.repos.d/fedora-modular.repo https://src.fedoraproject.org/rp
 curl -L -o /etc/yum.repos.d/fedora-updates-modular.repo https://src.fedoraproject.org/rpms/fedora-repos/raw/rawhide/f/fedora-updates-modular.repo
 curl -L -o /etc/yum.repos.d/group_redhat-et-microshift-fedora-35.repo https://copr.fedorainfracloud.org/coprs/g/redhat-et/microshift/repo/fedora-35/group_redhat-et-microshift-fedora-35.repo
 rpm-ostree ex module enable cri-o:1.21
-rpm-ostree install -y cri-o cri-tools microshift
+rpm-ostree install cri-o cri-tools microshift
 systemctl reboot
