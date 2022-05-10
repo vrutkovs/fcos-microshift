93a20c0945629d5f5135d1f0c6271cd21d355375
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-10 21:23:14 +0200

    Start microshift in bootstrap

--- bootstrap-microshift.sh
+++ bootstrap-microshift.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -eux
 
+systemctl enable --now microshift
+
 OPERATOR_SDK_VERSION="v1.20.0"
 
 curl -L -o /usr/local/bin/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk_linux_amd64
 chmod a+x /usr/local/bin/operator-sdk
 
+# TODO: wait for file to appear here
 cp -rvf /var/lib/microshift/resources/kubeadmin/kubeconfig /tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
 operator-sdk olm install
