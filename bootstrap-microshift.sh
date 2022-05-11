efe97d9102685fb7099a16da82df2509158cc08c
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-05-11 10:02:06 +0200

    bootstrap-microshift.sh: retry applying manifests and kubeconfig copy

--- bootstrap-microshift.sh
+++ bootstrap-microshift.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eux
 
+function retry {
+  local n=1
+  local max=5
+  local delay=15
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
 systemctl enable --now microshift
 
 OPERATOR_SDK_VERSION="v1.20.0"
@@ -9,13 +26,13 @@ curl -L -o /usr/local/bin/operator-sdk https://github.com/operator-framework/ope
 chmod a+x /usr/local/bin/operator-sdk
 
 # TODO: wait for file to appear here
-cp -rvf /var/lib/microshift/resources/kubeadmin/kubeconfig /tmp/kubeconfig
+retry cp -rvf /var/lib/microshift/resources/kubeadmin/kubeconfig /tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
 operator-sdk olm install
 
 oc create -f https://operatorhub.io/install/argocd-operator.yaml
 
-oc apply -f /etc/microshift/manifests/argocd-settings.yaml
-oc apply -f /etc/microshift/manifests/cluster-app.yaml
+retry oc apply -f /etc/microshift/manifests/argocd-settings.yaml
+retry oc apply -f /etc/microshift/manifests/cluster-app.yaml
 
 touch /var/lib/.microshift-provisioned
